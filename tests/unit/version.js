74c446ef3fd36a09b183cd62cfee824470769bfb
Rejith Ravindran <ravre03@icloud.com>
2019-06-12 10:22:50 +1000

    Update version.js

--- tests/unit/version.js
+++ tests/unit/version.js
@@ -5,6 +5,6 @@ const code = require('../../components/version.js')
 describe('version-unit', () => {
   it("should return {'version': 1.0.0}", () => {
     const actual = JSON.stringify(code.version())
-    expect(actual).to.equal(JSON.stringify({ 'version': 1.0.0 }))
+    expect(actual).to.equal(JSON.stringify({ 'version': 1.0 }))
   })
 })
