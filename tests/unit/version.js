024dba1446fb0ffb30736fcef41033b457f5dcb9
Rejith Ravindran <ravre03@icloud.com>
2019-06-12 10:18:29 +1000

    Update version.js

--- tests/unit/version.js
+++ tests/unit/version.js
@@ -3,8 +3,8 @@ const expect = chai.expect
 const code = require('../../components/version.js')
 
 describe('version-unit', () => {
-  it("should return {'version': 1.0}", () => {
+  it("should return {'version': 1.0.0}", () => {
     const actual = JSON.stringify(code.version())
-    expect(actual).to.equal(JSON.stringify({ 'version': 0.1 }))
+    expect(actual).to.equal(JSON.stringify({ 'version': 1.0.0 }))
   })
 })
