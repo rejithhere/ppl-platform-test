4e33691b311604ce98ecd4deb231dd079ee8ba0b
reravindran <reravindran@deloitte.com.au>
2019-06-12 13:18:31 +1000

    code clean up

--- my-simple-app.js
+++ my-simple-app.js
@@ -10,18 +10,7 @@ var revision = require('child_process')
 
 const port = 8080
 
-// Business logic sits under 'components' directory; need to unit test these
-const version = require('./components/version.js')
-
 app.get('/version', function (req, res) {
-  res.send(version.version())
-})
-
-app.get('/', function (req, res) {
-  res.send(homepage.content())
-})
-
-app.get('/about', function (req, res) {
   return res.json({
 PlatformTestApp: [{
 "version": pjson.version,
